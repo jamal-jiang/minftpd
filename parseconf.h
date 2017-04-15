bdbcfb85242d2f504ac4b4cfe63a0381ffbe7a34
yuxin.jyx <yuxin.jyx@alibaba-inc.com>
2017-04-15 22:42:18 +0800

    add parse conf file

---
+++ parseconf.h
@@ -0,0 +1,12 @@
+/**
+* auther: jamal-jiang
+* date: 17/4/15
+*/
+
+#ifndef MINFTPD_PARSECONF_H
+#define MINFTPD_PARSECONF_H
+
+void parseconf_load_file(const char *path);
+void parseconf_load_setting(const char *setting);
+
+#endif //MINFTPD_PARSECONF_H
